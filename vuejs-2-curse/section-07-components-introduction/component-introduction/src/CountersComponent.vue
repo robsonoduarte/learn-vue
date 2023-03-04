b8fb9445f929085f07a0f01488e9dae43c1582a3
Robson Duarte <robosn.o.d@gmail.com>
2023-03-03 23:29:13 -0300

    start section 07 introdution to vue components

---
+++ vuejs-2-curse/section-07-components-introduction/component-introduction/src/CountersComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Counters</h1>
+    <app-counter v-for="k in 5" :key="k"/>
+  </div>
+</template>
+
+<script>
+import Counter from './CounterComponent.vue'
+
+export default {
+  components: {
+    'app-counter': Counter
+  }
+}
+</script>
+
+<style>
+
+</style>
