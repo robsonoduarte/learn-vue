5d1fed4034883f7060d4caadf9f3f5d94d9512cd
Robson Duarte <robosn.o.d@gmail.com>
2023-03-15 23:30:14 -0300

    :muscle:

--- vuejs-2-curse/section-08-components-communication/component-communication/src/components/User.vue
+++ vuejs-2-curse/section-08-components-communication/component-communication/src/components/User.vue
@@ -1,12 +1,14 @@
 <template>
-    <div class="container">
-        <h1>User Component</h1>
-        <hr>
-        <div class="components">
-            <app-user-info v-bind:name="name"/>
-            <app-user-edit />
-        </div>
+  <div class="container">
+    <h1>User Component</h1>
+    <p><strong>{{ name }}</strong></p>
+    <button @click="changeName">Change Name</button>
+    <hr>
+    <div class="components">
+      <app-user-info v-bind:name="name" @nameChanged="name= $event" :reboot-name-callback="rebootName"/>
+      <app-user-edit/>
     </div>
+  </div>
 </template>
 
 <script>
@@ -14,31 +16,39 @@ import AppUserInfo from './UserInfo.vue'
 import AppUserEdit from './UserEdit.vue'
 
 export default {
-    components: { AppUserInfo, AppUserEdit },
-    data(){
-      return {
-        name: 'Bravox'
-      }
+  components: {AppUserInfo, AppUserEdit},
+  data() {
+    return {
+      name: 'Bravox'
     }
+  },
+  methods: {
+    changeName() {
+      this.name = this.name === 'Bravox' ? "Ana Mara" : 'Bravox'
+    },
+    rebootName() {
+      this.name = 'Bravox'
+    }
+  }
 }
 </script>
 
 <style scoped>
-    .container {
-        background-color: #27363b;
-        color: #fff;
-        padding: 10px;
-    }
+.container {
+  background-color: #27363b;
+  color: #fff;
+  padding: 10px;
+}
 
-    .container hr {
-        margin: 20px 10px;
-    }
+.container hr {
+  margin: 20px 10px;
+}
 
-    .components {
-        display: flex;
-    }
+.components {
+  display: flex;
+}
 
-    .components > * {
-        margin: 10px;
-    }
+.components > * {
+  margin: 10px;
+}
 </style>
--- vuejs-2-curse/section-08-components-communication/component-communication/src/components/UserInfo.vue
+++ vuejs-2-curse/section-08-components-communication/component-communication/src/components/UserInfo.vue
@@ -1,22 +1,38 @@
 <template>
-    <div class="component">
-        <h2>User Information</h2>
-        <p>Name: {{name}}</p>
-    </div>
+  <div class="component">
+    <h2>User Information</h2>
+    <p>Name: {{ reverseName() }}</p>
+    <button @click="rebootName">Reboot Name (Event)</button>
+    <button @click="rebootNameCallback">Reboot Name (Callback)</button>
+  </div>
 </template>
 
 <script>
 export default {
-    props: [
-        'name'
-    ]
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    rebootNameCallback: Function
+  },
+  methods: {
+    reverseName() {
+      return this.name.toUpperCase()
+    },
+    rebootName() {
+      this.name = 'Bravox'
+      this.$emit('nameChanged', this.name)
+    }
+  }
+
 }
 </script>
 
 <style scoped>
-    .component {
-        flex: 1;
-        background-color: #ec485f;
-        color: #fff;
-    }
+.component {
+  flex: 1;
+  background-color: #ec485f;
+  color: #fff;
+}
 </style>
