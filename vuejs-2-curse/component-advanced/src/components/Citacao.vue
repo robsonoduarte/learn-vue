8730b90b96e176b8a9118dc2c0d01fabe0b3db16
Robson Duarte <robson.o.d@gmail.com>
2023-12-15 01:07:41 -0300

    start component advanced chapter (#27)

---
+++ vuejs-2-curse/component-advanced/src/components/Citacao.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="citacao">
+    <slot name="author"></slot>
+    <slot name="text"></slot>
+    <slot name="font"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+.citacao {
+  border: 1px solid #DDD;
+  background-color: rgba(0, 0, 0, .1);
+  padding: 25px;
+  max-width: 450px;
+}
+</style>
