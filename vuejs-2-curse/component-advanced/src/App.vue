9791d2f21fc4edcdc2de8bfea57042c8c75b6bde
Robson Duarte <robson.o.d@gmail.com>
2023-12-20 00:11:59 -0300

    dinamic components (#30)

--- vuejs-2-curse/component-advanced/src/App.vue
+++ vuejs-2-curse/component-advanced/src/App.vue
@@ -1,8 +1,13 @@
 <template>
-	<div id="app">
-		<Citacoes />
-		<Sobre />
-	</div>
+  <div id="app">
+    <span>
+      <button @click="component = 'Citacoes'">Citacões</button>
+      <button @click="component = 'Sobre'">Sobre</button>
+    </span>
+    <keep-alive>
+      <component :is="component"/>
+    </keep-alive>
+  </div>
 </template>
 
 <script>
@@ -10,28 +15,33 @@ import Citacoes from './components/Citacoes'
 import Sobre from './components/Sobre'
 
 export default {
-	components: { Citacoes, Sobre }
+  components: {Citacoes, Sobre},
+  data() {
+    return {
+      component: Citacoes
+    }
+  }
 }
 </script>
 
 <style>
-	#app {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		height: 100vh;
-		font-size: 1.6rem;
-	}
+#app {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  font-size: 1.6rem;
+}
 
-	button {
-        outline: none;
-        padding: 5px 15px 10px;
-        margin: 10px 5px;
-        border-radius: 3px;
-        font-size: 2rem;
-        font-weight: 300;
-        color: #FFF;
-        background-color: rgba(0, 0, 0, .5);
-    }
+button {
+  outline: none;
+  padding: 5px 15px 10px;
+  margin: 10px 5px;
+  border-radius: 3px;
+  font-size: 2rem;
+  font-weight: 300;
+  color: #FFF;
+  background-color: rgba(0, 0, 0, .5);
+}
 </style>
