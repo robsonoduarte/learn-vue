10596e258cbe61d52e0244eaaf371c403bf03e80
Robson Duarte <robson.o.d@gmail.com>
2023-12-21 00:54:50 -0300

    task project - components taks and newtask (#34)

--- vuejs-2-curse/tasks/src/components/TaskGrid.vue
+++ vuejs-2-curse/tasks/src/components/TaskGrid.vue
@@ -1,20 +1,36 @@
 <template>
-  <div class="tasks">
-    <template>
-      <div v-for="task in tasks" :key="task.name">{{ task.name }}</div>
+  <div class="tasks-grid">
+    <template v-if="tasks.length">
+      <task :task="task" v-for="task in tasks" :key="task.name"></task>
     </template>
+    <p v-else class="no-task">Your life is up to date</p>
   </div>
 </template>
 
-
 <script>
+import Task from "@/components/Task.vue";
+
 export default {
+  components: {Task},
   props: {
     tasks: {type: Array, required: true}
   }
 }
 </script>
 
-<style scoped>
+<style>
+.tasks-grid {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
 
+.tasks-grid .task {
+  margin: 10px;
+}
+
+.no-task {
+  color: #AAA;
+  font-size: 1.7rem;
+}
 </style>
