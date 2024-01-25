4b1ea54c206e84766f8fab7adffe3a78cd7d33d6
Robson Duarte <robson.o.d@gmail.com>
2024-01-25 01:02:04 -0300

    start customs directives chapter (#40)

---
+++ vuejs-2-curse/directives/README.md
@@ -0,0 +1,23 @@
+# vue.js custom directives
+
+
+### the topics are:
+
+* [create]()
+* [using ]()
+* [passing value]()
+* [using value]()
+* [passing argument]()
+* [using argument]()
+* [passing modifiers]()
+* [using modifiers]()
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
---
+++ vuejs-2-curse/directives/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <h1>Directives</h1>
+    <hr/>
+    <p v-highlight="color">Using the custom directive</p>
+    <p v-highlight:back="'lightblue'">Using the custom directive</p>
+    <p v-highlight:back.delay="'lightgreen'">Using the custom directive</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      color: 'green'
+    }
+  }
+}
+</script>
+
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 2.5rem;
+}
+</style>
