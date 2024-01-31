0b7dd5750a8cb641d6584c38dd0345d962002233
Robson Duarte <robson.o.d@gmail.com>
2024-01-31 00:09:53 -0300

    directives - local directive (#43)

--- vuejs-2-curse/directives/src/App.vue
+++ vuejs-2-curse/directives/src/App.vue
@@ -5,11 +5,28 @@
     <p v-highlight="color">Using the custom directive</p>
     <p v-highlight:back="'lightblue'">Using the custom directive</p>
     <p v-highlight:back.delay="'lightgreen'">Using the custom directive</p>
+    <p v-highlight-local:back.delay="'lightgreen'">Using the custom local directive</p>
   </div>
 </template>
 
 <script>
 export default {
+  directives:{
+     'highlight-local':{
+       bind(el, binding, vnode){
+         let delay = 0;
+         if (binding.modifiers['delay']) delay = 3000;
+         setTimeout(() => {
+           if (binding.arg === 'back') {
+             el.style.backgroundColor = binding.value;
+           } else {
+             el.style.color = binding.value;
+           }
+         }, delay);
+
+       }
+     }
+  },
   data() {
     return {
       color: 'green'
