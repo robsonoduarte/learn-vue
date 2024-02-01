937005481fa9254a0e0293d4cea335152b2d0419
robsonoduarte <robson.o.d@gmail.com>
2024-02-01 00:39:45 -0300

    directives - passing object as value

--- vuejs-2-curse/directives/src/App.vue
+++ vuejs-2-curse/directives/src/App.vue
@@ -7,6 +7,7 @@
     <p v-highlight:back.delay="'lightgreen'">Using the custom directive with modifiers </p>
     <p v-highlight-local:back.delay="'lightgreen'">Using the custom local directive</p>
     <p v-highlight-local:back.delay.toggle="'lightgreen'">Using the custom local directive with multiple modifiers</p>
+    <p v-highlight-local-object:back.delay.toggle="{color: 'lightgreen', colorToggle: 'mediumpurple', delay: 5000, interval: 2000}">Using the custom local directive passing a object</p>
   </div>
 </template>
 
@@ -36,7 +37,32 @@ export default {
           }
         }, delay);
 
-      }
+      },
+    },
+    'highlight-local-object': {
+      bind(el, binding, vnode) {
+        const applyColor = (color) => {
+          if (binding.arg === 'back') {
+            el.style.backgroundColor = color;
+          } else {
+            el.style.color = color;
+          }
+        }
+        let delay = 0;
+        if (binding.modifiers['delay']) delay = binding.value.delay;
+        setTimeout(() => {
+          let color = binding.value.color;
+          if (binding.modifiers['toggle']){
+            setInterval(() => {
+              color = color === binding.value.color ? binding.value.colorToggle : binding.value.color;
+              applyColor(color);
+            }, binding.value.interval);
+          }else {
+            applyColor(color);
+          }
+        }, delay);
+
+      },
     }
   },
   data() {
