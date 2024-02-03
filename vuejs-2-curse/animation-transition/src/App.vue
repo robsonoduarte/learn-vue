b8c0c0cfe4de8cab4d077fc694686e005f68952c
Robson Duarte <robson.o.d@gmail.com>
2024-02-03 01:11:45 -0300

    animation/transition - start the chapter (#54)

---
+++ vuejs-2-curse/animation-transition/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app" class="container-fluid">
+    <h1>Animations & Transitions</h1>
+    <hr>
+    <b-button variant="primary" class="mb-4" @click="show = !show">Show Message</b-button>
+    <transition>
+      <b-alert variant="info" show v-if="show">{{ message }}</b-alert>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'A message of information for the user',
+      show: false,
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 1.5rem;
+}
+</style>
