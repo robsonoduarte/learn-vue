992254f9e6e636b18cdae8b1318dc80d22ea052e
Robson Duarte <robson.o.d@gmail.com>
2024-02-28 00:21:37 -0300

    animation/transition - organize layout (#62)

--- vuejs-2-curse/animation-transition/src/App.vue
+++ vuejs-2-curse/animation-transition/src/App.vue
@@ -2,34 +2,66 @@
   <div id="app" class="container-fluid">
     <h1>Animations & Transitions</h1>
     <hr>
-    <b-button variant="primary" class="mb-4" @click="show = !show">Show Message</b-button>
-    <transition name="fade">
-      <b-alert variant="dark" show v-if="show">Fade Animation</b-alert>
-    </transition>
-    <transition name="slide">
-      <b-alert variant="info" show v-if="show">Slide Animation</b-alert>
-    </transition>
-    <transition name="fade-slide">
-      <b-alert variant="danger" show v-if="show">Fade and Slide - Mixing the Transition and Animation CSS </b-alert>
-    </transition>
-    <transition name="fade" appear>
-      <b-alert variant="secondary" show v-if="!show">Using the appear to loading animation with the component</b-alert>
-    </transition>
-    <transition
-        enter-active-class="animated bounce"
-        leave-active-class="animated shake">
-      <b-alert variant="primary" show v-if="show">Using external css to Animation</b-alert>
-    </transition>
+    <b-container>
+      <b-row>
+        <b-col>
+          <b-nav vertical>
+            <b-nav-item @click="column = 'fade'" >Fade</b-nav-item>
+            <b-nav-item @click="column = 'slide'">Slide</b-nav-item>
+            <b-nav-item @click="column = 'fade-slide'">Fade/Slide Mixing Transition</b-nav-item>
+            <b-nav-item @click="column = 'appear'">Animation loading component</b-nav-item>
+            <b-nav-item @click="column = 'external'">External CSS</b-nav-item>
+            <b-nav-item @click="column = 'two-way'">two-way binding / multiples component</b-nav-item>
+          </b-nav>
+        </b-col>
+        <b-col>
+          <b-col v-if="column === 'fade'">
+            <b-button variant="primary" class="mb-4" @click="show = !show" >Fade</b-button>
+            <transition name="fade">
+              <b-alert variant="dark" show v-if="show">Fade Animation</b-alert>
+            </transition>
+          </b-col>
+          <b-col v-else-if="column === 'slide'">
+            <b-button variant="primary" class="mb-4" @click="show = !show" >Slide</b-button>
+            <transition name="slide">
+              <b-alert variant="info" show v-if="show">Slide Animation</b-alert>
+            </transition>
+          </b-col>
+          <b-col v-else-if="column === 'fade-slide'">
+            <b-button variant="primary" class="mb-4" @click="show = !show" >Fade/Slide</b-button>
+            <transition name="fade-slide">
+              <b-alert variant="danger" show v-if="show">Fade and Slide - Mixing the Transition and Animation CSS </b-alert>
+            </transition>
+          </b-col>
+          <b-col v-else-if="column === 'appear'">
+            <b-button variant="primary" class="mb-4" @click="show = !show" >Appear</b-button>
+            <transition name="fade" appear>
+              <b-alert variant="secondary" show v-if="!show">Using the appear to loading animation with the component</b-alert>
+            </transition>
+          </b-col>
+          <b-col v-else-if="column === 'external'">
+            <b-button variant="primary" class="mb-4" @click="show = !show" >External</b-button>
+            <transition
+                enter-active-class="animated bounce"
+                leave-active-class="animated shake">
+              <b-alert variant="primary" show v-if="show">Using external css to Animation</b-alert>
+            </transition>
+          </b-col>
+          <b-col v-else-if="column === 'two-way'">
+            <b-button variant="primary" class="mb-4" @click="show = !show" >Two-Way</b-button>
+            <b-select v-model="animationType" class="mb-4">
+              <option value="fade">Fade</option>
+              <option value="slide">Slide</option>
+            </b-select>
+            <transition :name="animationType" mode="out-in">
+              <b-alert variant="info" show v-if="show" key="info"> Using two-way binding and multiples component {{animationType.toUpperCase()}}</b-alert>
+              <b-alert variant="success" show v-else key="success"> Using two-way binding and multiples component {{animationType.toUpperCase()}}</b-alert>
+            </transition>
+          </b-col>
+        </b-col>
+      </b-row>
+    </b-container>
 
-    <hr>
-    <b-select v-model="animationType" class="mb-4">
-      <option value="fade">Fade</option>
-      <option value="slide">Slide</option>
-    </b-select>
-    <transition :name="animationType" mode="out-in">
-      <b-alert variant="info" show v-if="show" key="info"> Using two-way binding and multiples component {{animationType.toUpperCase()}}</b-alert>
-      <b-alert variant="success" show v-else key="success"> Using two-way binding and multiples component {{animationType.toUpperCase()}}</b-alert>
-    </transition>
   </div>
 </template>
 
@@ -38,6 +70,7 @@ export default {
   data() {
     return {
       show: false,
+      column: '',
       animationType: 'fade'
     }
   }
