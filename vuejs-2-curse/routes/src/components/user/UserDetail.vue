7aeec7423ed1853dbfd5cf7ec47c13e40f4c7fae
robsonoduarte <robson.o.d@gmail.com>
2024-03-20 00:23:51 -0300

    vue router - redirect animante hash

--- vuejs-2-curse/routes/src/components/user/UserDetail.vue
+++ vuejs-2-curse/routes/src/components/user/UserDetail.vue
@@ -4,7 +4,7 @@
     <hr>
     <p><strong>Id: </strong>{{ id }}</p>
     <!--<router-link tag="button" primary :to="`/user/${id}/edit`">Edit</router-link>-->
-    <router-link tag="button" primary :to="{name: 'userEdit', params: {id}, query: {country: 'Brazil', language: 'pt_br' }}">Edit</router-link>
+    <router-link tag="button" primary :to="{name: 'userEdit', params: {id}, query: {country: 'Brazil', language: 'pt_br'}, hash: '#footer'}">Edit</router-link>
   </div>
 </template>
 
