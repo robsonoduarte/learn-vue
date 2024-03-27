c148461d918e2a2323cbdc055565de69c227dceb
robsonoduarte <robson.o.d@gmail.com>
2024-03-27 00:53:46 -0300

    vuex - getters

--- vuejs-2-curse/vuex/src/store/store.js
+++ vuejs-2-curse/vuex/src/store/store.js
@@ -6,5 +6,12 @@ Vue.use(Vuex)
 export default new Vuex.Store({
     state: {
         products: []
+    },
+    getters: {
+        amount(state) {
+            return state.products
+                .map(p => p.amount * p.price)
+                .reduce((total, current) => total + current, 0)
+        },
     }
 })
