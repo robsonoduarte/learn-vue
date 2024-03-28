e046cbcf747016cfd45ce8902d0053858ab031a7
Robson Duarte <robson.o.d@gmail.com>
2024-03-28 01:07:44 -0300

    vuex - mutations and actions (#95)

--- vuejs-2-curse/vuex/src/components/Parameters.vue
+++ vuejs-2-curse/vuex/src/components/Parameters.vue
@@ -1,32 +1,44 @@
 <template>
-    <Panel title="Parâmetros" vermelho>
-        <div class="parametros">
+  <Panel title="Parameters" vermelho>
+    <div class="parameters">
             <span>
-                <strong>Quantidade Padrão: </strong> 
-                <input type="number" v-model="quantidade">
+                <strong>Standard Amount: </strong>
+                <input type="number" v-model="amount">
             </span>
-            <span>
-                <strong>Preço Padrão: </strong>
-                <input type="number" v-model="preco">
+      <span>
+                <strong>Standard Price: </strong>
+                <input type="number" v-model="price">
             </span>
-        </div>
-    </Panel>
+    </div>
+  </Panel>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            quantidade: 0,
-            preco: 0
-        }
+  computed: {
+    amount:{
+      get() {
+        return this.$store.state.amount
+      },
+      set(value) {
+        this.$store.commit("setAmount", value)
+      },
+    },
+    price:{
+      get() {
+        return this.$store.state.price
+      },
+      set(value) {
+        this.$store.commit("setPrice", value)
+      }
     }
+  }
 }
 </script>
 
 <style>
-    .parametros {
-        display: flex;
-        justify-content: space-around;
-    }
+.parameters {
+  display: flex;
+  justify-content: space-around;
+}
 </style>
--- vuejs-2-curse/vuex/src/components/Shop.vue
+++ vuejs-2-curse/vuex/src/components/Shop.vue
@@ -1,52 +1,64 @@
 <template>
-    <Panel title="Virtual Shop" verde>
-        <div class="shop">
-            <span>Add</span>
-            <input type="number" v-model.number="amount">
-            <span>items of <strong>R$</strong></span>
-            <input type="number" v-model.number="price">
-            <button @click="add">Now!</button>
-        </div>
-    </Panel>
+  <Panel title="Virtual Shop" verde>
+    <div class="shop">
+      <span>Add</span>
+      <input type="number" v-model.number="amount">
+      <span>items of <strong>R$</strong></span>
+      <input type="number" v-model.number="price">
+      <button @click="add">Now!</button>
+    </div>
+  </Panel>
 </template>
 
 <script>
+// eslint-disable-next-line no-unused-vars
+import {mapActions, mapMutations} from "vuex";
 
 export default {
-    data() {
-        return {
-            sequence: 1,
-            amount: 1,
-            price: 9.99,
-        }
-    },
-    methods: {
-        add() {
-            const product = {
-                id: this.sequence,
-                name: `Product ${this.sequence}`,
-                amount: this.amount,
-                price: this.price
-            }
-            this.sequence++
-            this.$store.state.products.push(product)
-        }
+  data() {
+    return {
+      sequence: 1,
     }
+  },
+  computed: {
+    amount() {
+      return this.$store.state.amount
+    },
+    price() {
+      return this.$store.state.price
+    },
+  },
+  methods: {
+    add() {
+      const product = {
+        id: this.sequence,
+        name: `Product ${this.sequence}`,
+        amount: this.amount,
+        price: this.price
+      }
+      this.sequence++
+      //this.$store.commit('addProduct', product)
+      //this.$store.dispatch("addProduct", product)
+      this.addProduct(product)
+    },
+    //...mapMutations(["addProduct"])
+    ...mapActions(['addProduct'])
+  }
 }
 </script>
 
 <style>
-    .shop {
-        display: flex;
-        justify-content: center;
-    }
+.shop {
+  display: flex;
+  justify-content: center;
+}
 
-    .shop > * {
-        margin: 0px 10px;
-    }
+.shop > * {
+  margin: 0px 10px;
+}
 
-    input {
-        font-size: 2rem;
-        width: 90px;
-    }
+input {
+  font-size: 2rem;
+  width: 90px;
+}
 </style>
