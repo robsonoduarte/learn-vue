cd48ab7d0c45bcaa88de134f37d9e59ff922f828
robsonoduarte <robson.o.d@gmail.com>
2024-04-06 00:58:10 -0300

    stock trader - header component

--- vuejs-2-curse/stock-trader/src/components/Header.vue
+++ vuejs-2-curse/stock-trader/src/components/Header.vue
@@ -1,5 +1,30 @@
 <template>
-  <h1>Header</h1>
+  <v-toolbar app>
+    <v-toolbar-title class="headline text-uppercase mr-4">
+      <span>Stock</span>
+      <span class="font-weight-light">Trader</span>
+    </v-toolbar-title>
+    <v-toolbar-items>
+      <v-btn flat to="/">Home</v-btn>
+      <v-btn flat to="/portfolio">Portfolio</v-btn>
+      <v-btn flat to="/stocks">Stocks</v-btn>
+    </v-toolbar-items>
+    <v-spacer/>
+    <v-toolbar-items>
+      <v-btn flat>End Day</v-btn>
+      <v-menu offset-y>
+        <v-btn flat slot="activator">Save & Load</v-btn>
+        <v-list>
+          <v-list-tile>
+            <v-list-tile-title>Save Data</v-list-tile-title>
+          </v-list-tile>
+          <v-list-tile>
+            <v-list-tile-title>Load Data</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+    </v-toolbar-items>
+  </v-toolbar>
 </template>
 
 <script>
