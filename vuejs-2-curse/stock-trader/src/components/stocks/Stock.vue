bb227e120d41a86503f8afc199bc113e7a6d5c11
robsonoduarte <robson.o.d@gmail.com>
2024-04-12 00:48:05 -0300

    stock trader - stock component | vuex

--- vuejs-2-curse/stock-trader/src/components/stocks/Stock.vue
+++ vuejs-2-curse/stock-trader/src/components/stocks/Stock.vue
@@ -1,8 +1,42 @@
 <template>
-  <h1>Stock</h1>
+  <v-flex class="pb-3 pr-3" x12 md6 lg4>
+    <v-card class="green darken-3 white--text">
+      <v-card-title class="headline">
+        <strong>{{ stock.name }} <small>Price: {{ stock.price }}</small></strong>
+      </v-card-title>
+    </v-card>
+    <v-card>
+      <v-container fill-height>
+        <v-text-field label="Amount" type="number" v-model.number="amount"/>
+        <v-btn class="green darken-3 withe--text" @click="buy" :disabled="amount <= 0 || !Number.isInteger(amount)">
+          Buy
+        </v-btn>
+      </v-container>
+    </v-card>
+  </v-flex>
 </template>
 
 <script>
+export default {
+  props: ['stock'],
+  data() {
+    return {
+      amount: 0
+    }
+  },
+  methods: {
+    buy() {
+      const order = {
+        id: this.stock.id,
+        price: this.stock.price,
+        amount: this.amount,
+      }
+      // eslint-disable-next-line
+      console.log(order)
+      this.amount = 0
+    }
+  }
+}
 </script>
 
 <style>
--- vuejs-2-curse/stock-trader/src/components/stocks/Stocks.vue
+++ vuejs-2-curse/stock-trader/src/components/stocks/Stocks.vue
@@ -1,9 +1,23 @@
 <template>
-  <h1>Stocks</h1>
+  <v-layout row wrap>
+    <Stock v-for="stock in stocks" :key="stock.id" :stock="stock"/>
+  </v-layout>
 </template>
 
 
 <script>
+
+import Stock from './Stock'
+
+export default {
+  components: {Stock},
+  computed:{
+    stocks(){
+      return this.$store.getters.stocks
+    }
+  }
+}
+
 </script>
 
 <style>
