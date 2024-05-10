8081baa0dd9958df17aaba3749fd320493aeb9d0
robsonoduarte <robson.o.d@gmail.com>
2024-05-10 00:53:24 -0300

    calculator - finish the project

--- vuejs-2-curse/calculator/src/components/Button.vue
+++ vuejs-2-curse/calculator/src/components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="button" :class="{double, triple, operation}">
+  <button class="button" :class="{double, triple, operation}" @click="$emit('buttonClick', label)">
     {{ label }}
   </button>
 </template>
