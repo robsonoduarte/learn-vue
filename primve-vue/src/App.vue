5d9ebcb2f2c5337a12c7a8be24b997ac2d8fcd7d
robsonoduarte <robson.o.d@gmail.com>
2024-06-28 00:34:33 -0300

    prime-vue - using the chart primevue component

--- primve-vue/src/App.vue
+++ primve-vue/src/App.vue
@@ -1,19 +1,71 @@
-<script >
-import DatePicker from 'primevue/datepicker';
+<template>
+  <Chart type="line" :data="data" :options="options"></Chart>
+</template>
+
+
+<script>
+import Chart from 'primevue/chart'
+
 export default {
-  components: {DatePicker},
-  data(){
-    return{
-      date: ''
+  components: { Chart },
+  data() {
+    return {
+      data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+          {
+            label: 'dataset 1',
+            backgroundColor: '#144e86',
+            borderColor: '#5454a8',
+            data: [65, 59, 80, 81, 56, 55, 40]
+          },
+          {
+            label: 'dataset 2',
+            backgroundColor: '#FA8072',
+            borderColor: '#FF0000',
+            data: [-10, 4, 60, 15, 30, 55, 80]
+          },
+          {
+            label: 'dataset 3',
+            backgroundColor: '#F0FFF0',
+            borderColor: '#FFFFFF',
+            data: [-0, 10, 33, -15, 30, 60, 70]
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top'
+          },
+          title: {
+            display: true,
+            text: 'Prime Vue Chart.js'
+          },
+        },
+        scales: {
+          x: {
+            grid: {
+              display: true,
+              color: '#FFFFFF',
+              lineWidth: 0.1
+            }
+          },
+          y: {
+            grid: {
+              display: true,
+              color: '#FFFFFF',
+              lineWidth: 0.05
+            }
+          }
+        }
+      }
     }
   }
 }
-
 </script>
 
-<template>
-  <DatePicker v-model="date" />
-</template>
 
 <style scoped>
 </style>
